fe02430f5b0fb535c44666b5d7f06988e6148e7c
Josh Tefay <josh@macroview.com.au>
2024-07-23 10:34:44 +1000

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Identity.Client;
+using Microsoft.Identity.Client.Desktop;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace MsalWpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {        
+        private const string CommonAuthorityUri = "https://login.microsoftonline.com/common";
+        private const string ClientId = "d3590ed6-52b3-4102-aeff-aad2292ab01c";
+        private const string RedirectUri = "urn:ietf:wg:oauth:2.0:oob";
+        private const string GraphResourceUri = "https://graph.microsoft.com";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                AuthenticationResult result = await AcquireTokenAsync(new[] { $"{GraphResourceUri}/.default" }, true);
+                SignInResult.Text = $"Signed in as {result.Account.Username}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(GetWindow(this), ex.ToString(), $"Error: {ex.Message}", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private async Task<AuthenticationResult> AcquireTokenAsync(IEnumerable<string> scopes, bool forceInteractivePrompt = false)
+        {
+            PublicClientApplicationBuilder builder =
+                PublicClientApplicationBuilder.Create(ClientId)
+                .WithAuthority(CommonAuthorityUri)
+                .WithRedirectUri(RedirectUri)
+                .WithBroker(new BrokerOptions(BrokerOptions.OperatingSystems.Windows)); //WAM broker fails when process is elevated.
+
+            IPublicClientApplication pca = builder.Build();
+            IntPtr parentHwnd = new WindowInteropHelper(GetWindow(this)).Handle;
+            try
+            {
+                if (forceInteractivePrompt)
+                {
+                    return await pca.AcquireTokenInteractive(scopes).WithParentActivityOrWindow(parentHwnd).ExecuteAsync();
+                }
+                IEnumerable<IAccount> accounts = await pca.GetAccountsAsync();
+                return await pca.AcquireTokenSilent(scopes, accounts.FirstOrDefault()).ExecuteAsync();
+            }
+            catch (MsalUiRequiredException)
+            {
+                return await pca.AcquireTokenInteractive(scopes).WithParentActivityOrWindow(parentHwnd).ExecuteAsync();
+            }
+        }
+    }
+}
